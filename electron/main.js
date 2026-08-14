e32acdfac1662d072f788e8ce084a623a93c6e30
juancarlos-star <jcpublicidad4@gmail.com>
2026-08-14 11:57:05 +0200

    Update main.js

--- electron/main.js
+++ electron/main.js
@@ -25,13 +25,26 @@ function createWindow() {
   }
 }
 
-app.whenReady().then(() => {
-  initDb();
-  createWindow();
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+const gotTheLock = app.requestSingleInstanceLock();
+
+if (!gotTheLock) {
+  app.quit();
+} else {
+  app.on('second-instance', () => {
+    if (mainWindow) {
+      if (mainWindow.isMinimized()) mainWindow.restore();
+      mainWindow.focus();
+    }
   });
-});
+
+  app.whenReady().then(() => {
+    initDb();
+    createWindow();
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+  });
+}
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
