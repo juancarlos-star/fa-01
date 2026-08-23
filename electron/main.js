60f66a9e5bee48bfdace1b487cc6589b6237ac02
juancarlos-star <jcpublicidad4@gmail.com>
2026-08-23 14:28:29 +0200

    Add files via upload

--- electron/main.js
+++ electron/main.js
@@ -61,6 +61,8 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    title: 'MoviSync',
+    icon: path.join(__dirname, '..', 'build', 'icon.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
